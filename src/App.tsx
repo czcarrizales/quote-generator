4d999a7bdd02a912d6490899de633c677cfb892a
Chazz Carrizales <czcarrizales@yahoo.com>
2023-10-09 14:07:40 -0700

    rough skeleton of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [quote, setQuote] = useState('Choose a category, and press the button to generate a random quote!')
+  
+  const generateRandomQuote = async () => {
+    axios.get('https://api.quotable.io/quotes/random')
+    .then((response) => {
+      const randomQuote = response.data[0].content
+      setQuote(randomQuote)
+    })
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>"{quote}"</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <label htmlFor="category">Category</label>
+        <select name="category" id="category">
+          <option value="test">Test</option>
+        </select>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={generateRandomQuote}>Generate Quote</button>
     </>
   )
 }
