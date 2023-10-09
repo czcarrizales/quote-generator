4853416897051a2f5ebaa48d005ae8dfeff96a9f
Chazz Carrizales <czcarrizales@yahoo.com>
2023-10-09 14:49:53 -0700

    show author and handle category select

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,49 @@ import './App.css'
 function App() {
 
   const [quote, setQuote] = useState('Choose a category, and press the button to generate a random quote!')
+  const [author, setAuthor] = useState('Anonymous')
+  const [category, setCategory] = useState('')
+  const [loading, setLoading] = useState(false)
   
   const generateRandomQuote = async () => {
-    axios.get('https://api.quotable.io/quotes/random')
+    setLoading(true)
+    axios.get(`https://api.quotable.io/quotes/random?tags=${category}`)
     .then((response) => {
       const randomQuote = response.data[0].content
+      const randomQuoteAuthor = response.data[0].author
       setQuote(randomQuote)
+      setAuthor(randomQuoteAuthor)
+      setLoading(false)
     })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  const handleCategoryChange = (e: any) => {
+    setCategory(e.target.value)
   }
 
   return (
     <div className='app-main-container'>
       <div className='app-quote-container'>
-        <h1>"{quote}"</h1>
+        <h1>
+          {loading ? 'Getting your random quote!' : `"${quote}"`}</h1>
+          <h2>
+            {loading ? '' : `-${author}`}
+          </h2>
       </div>
       <div className='app-dropdown-container'>
         <label htmlFor="category">Category</label>
-        <select className='app-dropdown-select' name="category" id="category">
-          <option value="test">Test</option>
+        <select onChange={(e) => handleCategoryChange(e)} className='app-dropdown-select' name="category" id="category">
+          <option value="">All</option>
+          <option value="age">Age</option>
+          <option value="business">Business</option>
+          <option value="courage">Courage</option>
+          <option value="love">Love</option>
+          <option value="motivational">Motivational</option>
+          <option value="wisdom">Wisdom</option>
+
         </select>
       </div>
       <button className='app-dropdown-button' onClick={generateRandomQuote}>Generate Quote</button>
